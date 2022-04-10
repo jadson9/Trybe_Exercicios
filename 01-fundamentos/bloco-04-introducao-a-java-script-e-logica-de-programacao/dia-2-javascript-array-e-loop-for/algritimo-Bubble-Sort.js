166ea836604e3843d9a4597994203b9dfe0ae35f
Jadson Cerqueira <jadsoncerqueira97@gmail.com>
2022-04-10 17:29:20 -0300

    multiplica valores da lista pelo valor seguinte

--- 01-fundamentos/bloco-04-introducao-a-java-script-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/algritimo-Bubble-Sort.js
+++ 01-fundamentos/bloco-04-introducao-a-java-script-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/algritimo-Bubble-Sort.js
@@ -1,4 +1,6 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let newNumbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]
+let aux2 = []
 
 for (let i = 1; i < numbers.length; i += 1){
     for (let toi = 0; toi < i; toi += 1){
@@ -24,3 +26,11 @@ for (let i = 1; i < numbers.length; i += 1){
 }
 
 console.log("Ordem decrescente", numbers)
+
+for (let index = 0 ; index < newNumbers.length  ; index += 1) {
+        aux2.push(newNumbers[index] * newNumbers[index+1]) 
+    
+    
+}
+
+console.log("Valores multiplicados pelo valor seguinte", aux2)
