c33ea888b6d6eae1227d88d1841de4ab95c27f37
Jadson Cerqueira <jadsoncerqueira97@gmail.com>
2022-04-09 23:35:14 -0300

    cria programa que mostra função de peça de xadrex

---
+++ 01-fundamentos/bloco-04-introducao-a-java-script-e-logica-de-programacao/dia-1-javascript-primeiros-passos/funções-peças-de-xadres.js
@@ -0,0 +1,31 @@
+let peca = "carro";
+
+switch (peca){
+
+    case "peão":
+        console.log("Movimentos: somente para frente, sendo uma casa de cada vez, \n se for a primeira jogada da peça, pode avançar até duas casas")
+        break
+    
+    case "torre":
+        console.log("Movimentos: Horizontais e Verticais para frete e para trás")
+        break
+
+    case "cavalo":
+        console.log("Movimentos: Movimentos em L")
+        break
+
+    case "bispo":
+        console.log("Movimentos: Diagonais para frente e para tras")
+        break
+
+    case "rainha":
+        console.log("Movimentos: Horizontais, verticais e diagonais para frete e para tras")
+        break
+    
+    case "rei":
+        console.log("Movimentos: Horizontais, verticais e diagonais para frete e para tras com limite de apenas uma casa ")
+        break
+
+    default:
+        console.log("Erro: Nome inválido")
+}
