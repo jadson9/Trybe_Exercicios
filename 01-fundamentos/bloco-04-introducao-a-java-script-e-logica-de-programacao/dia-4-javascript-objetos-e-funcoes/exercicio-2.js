83568d7ec0829a71788bfbe59131c3b8abe50378
Jadson Cerqueira <jadsoncerqueira97@gmail.com>
2022-04-17 18:54:57 -0300

    adiciona livro favorito a lista

--- 01-fundamentos/bloco-04-introducao-a-java-script-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio-2.js
+++ 01-fundamentos/bloco-04-introducao-a-java-script-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio-2.js
@@ -16,3 +16,10 @@ let sobreNome = leitor['sobrenome']
 
 
 console.log('O livro favorito de '+ nome + ' ' + sobreNome + ' se chama ' + leitor.livrosFavoritos[0].titulo)
+leitor.livrosFavoritos.push({
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editora: 'Rocco',
+  })
+
+console.log(leitor.livrosFavoritos)
