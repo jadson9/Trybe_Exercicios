ccd0d954beeba42a31ca2fc53f812fd13594343c
Jadson Cerqueira <jadsoncerqueira97@gmail.com>
2022-04-17 18:16:29 -0300

    cria novo objetoe mostra seu valor

--- 01-fundamentos/bloco-04-introducao-a-java-script-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio-1.js
+++ 01-fundamentos/bloco-04-introducao-a-java-script-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio-1.js
@@ -14,3 +14,22 @@ for (x in info) {
     console.log(info[x])
 }
 
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
+    nota: 'O último MacPatinhas',
+  };
+info2['recorrente'] = 'Sim'
+
+for (let x2 in info){
+    if (x2 === 'recorrente'){
+        if(info[x] === info2[x]){
+        console.log('Ambos recorrentes')
+        }
+    } else {
+        console.log(info[x2], ' e ', info2[x2])
+    }
+    
+    
+}
+
