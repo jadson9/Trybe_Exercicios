cad44bdeaf4eceab45ccb9c3de503dbe49460f64
Jadson Cerqueira <jadsoncerqueira97@gmail.com>
2022-04-24 19:52:07 -0300

    Função que adiciona dinamicamente os dias do mes

--- 01-fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/scripts.js
+++ 01-fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/scripts.js
@@ -12,5 +12,19 @@ function createDaysOfTheWeek() {
   };
   
   createDaysOfTheWeek();
-  
+  let ulPai = document.querySelector('#days')
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  for (let i = 0; i < dezDaysList.length; i += 1) {
+    let dia = dezDaysList[i];
+    let diasSemana = document.createElement('li')
+    diasSemana.className = 'day'
+    if (dia === 24 || dia === 25 || dia === 31){
+      diasSemana.className = 'day holyday'
+    } else if(dia === 4 || dia === 11 || dia === 18 || dia === 25) {
+      diasSemana.className = 'day friday'
+    }
+    diasSemana.innerHTML = dia
+    ulPai.appendChild(diasSemana)
+
+  }
   // Escreva seu código abaixo.
