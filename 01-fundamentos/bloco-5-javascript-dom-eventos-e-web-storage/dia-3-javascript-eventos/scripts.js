cfa07f38a15af9dcd197e694ad65c25b4a1970aa
Jadson Cerqueira <jadsoncerqueira97@gmail.com>
2022-04-24 21:37:30 -0300

    Criando click no botao sexta feira que muda os numeros por nomes

--- 01-fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/scripts.js
+++ 01-fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/scripts.js
@@ -18,10 +18,12 @@ function createDaysOfTheWeek() {
     let dia = dezDaysList[i];
     let diasSemana = document.createElement('li')
     diasSemana.className = 'day'
-    if (dia === 24 || dia === 25 || dia === 31){
+    if (dia === 24 || dia === 31){
       diasSemana.className = 'day holiday'
-    } else if(dia === 4 || dia === 11 || dia === 18 || dia === 25) {
+    } else if(dia === 4 || dia === 11 || dia === 18) {
       diasSemana.className = 'day friday'
+    } else if (dia === 25 ){
+      diasSemana.className = 'day friday holiday'
     }
     diasSemana.innerHTML = dia
     ulPai.appendChild(diasSemana)
@@ -34,11 +36,46 @@ function createDaysOfTheWeek() {
   botao.id = "btn-holiday"
   botaoPai.appendChild(botao)
 
+  let interruptor = 0;
   botao.addEventListener("click", mudaCor )
   function mudaCor(){
     let dias = document.getElementsByClassName('holiday')
-    for (let i = 0; i < dias.length; i += 1) {
-      dias[i].style.backgroundColor = 'rgb(255, 255, 255)' 
+    if (interruptor === 0) {
+      for (let i = 0; i < dias.length; i += 1) {
+          dias[i].style.backgroundColor = 'white' 
+          interruptor = 1
+      }
+    } else if (interruptor === 1) {
+      for (let i = 0; i < dias.length; i += 1) {
+          dias[i].style.backgroundColor = 'rgb(238,238,238)' 
+          interruptor = 0
+      }
+    }
+    
+  }
+
+  let botao2Pai = document.querySelector('.buttons-container')
+  let botao2 = document.createElement('button')
+  botao2.innerText = 'Sexta-Feira'
+  botao2.id = "btn-friday"
+  botao2Pai.appendChild(botao2)
+
+  botao2.addEventListener("click", mudaNome )
+  let interruptor2 = 0;
+  let listAux = [];
+  function mudaNome(){
+    let dias2 = document.getElementsByClassName('friday')
+    if (interruptor2 == 0) {
+      for (let i = 0; i < dias2.length; i += 1) {
+        listAux.push(dias2[i].innerText)
+        dias2[i].innerText = '#Sexteouu!' 
+        interruptor2 = 1
+      }
+    } else if (interruptor2 == 1) {
+      for (let i = 0; i < dias2.length; i += 1) {
+        dias2[i].innerText = listAux[i]
+        interruptor2 = 0
+      }
     }
   }
 
