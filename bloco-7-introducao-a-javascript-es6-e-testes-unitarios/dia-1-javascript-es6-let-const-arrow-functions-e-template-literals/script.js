5173a8d60cf37dc9c398a3db24e579bc6376db72
Jadson Cerqueira <jadsoncerqueira97@gmail.com>
2022-05-10 11:31:23 -0300

    cria função de ordenamento de numeros

--- bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -11,3 +11,11 @@ const testingScope = (escopo) => {
   }
 
   testingScope(true);
+
+  let oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+  oddsAndEvens.sort(function(a, b) {
+    return a - b;
+  });
+
+  console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`); // será necessário alterar essa linha 😉
