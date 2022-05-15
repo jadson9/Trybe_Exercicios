483d231f32de35b433103f53202f39005a350357
Jadson Cerqueira <jadsoncerqueira97@gmail.com>
2022-05-15 14:16:25 -0300

    cria array com habilidades

--- bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/trocaPalavras.js
+++ bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/trocaPalavras.js
@@ -1,3 +1,5 @@
+let skills = ['html', 'css', 'JavaScript', 'Photoshop', 'Coreldraw']
+
 let trocaPalavra = (palavra, to = 'Bebeto') => {
     let resultado = palavra.replace('x', to )
     console.log(resultado)
