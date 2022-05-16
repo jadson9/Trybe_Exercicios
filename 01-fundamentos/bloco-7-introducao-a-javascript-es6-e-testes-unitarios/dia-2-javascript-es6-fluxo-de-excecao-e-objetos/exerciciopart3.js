56dbf025a66508ce149e1dc17f1d698cd9f8684a
Jadson Cerqueira <jadsoncerqueira97@gmail.com>
2022-05-16 12:44:58 -0300

    cria função que agrupa todos os objetos

--- 01-fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/exerciciopart3.js
+++ 01-fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/exerciciopart3.js
@@ -29,7 +29,6 @@ let mostraValores = (objeto) => Object.values(objeto)
 
 adicionaTurno(lesson2, 'turno', 'noite')
 
-
 console.log(lesson2)
 
 console.log(mostraChaves(lesson2))
@@ -37,4 +36,18 @@ console.log(mostraChaves(lesson2))
 console.log(mostraTamaho(lesson2))
 
 console.log(mostraValores(lesson2))
+
+
+let lessonsAll = (list) => {
+    let lessonsAllAux = {}
+    for (let i = 0; i < list.length ; i += 1) {
+        let objetoCopy = Object.assign({}, list[i]);
+        lessonsAllAux[`lesson${i + 1}`] = objetoCopy
+    }
+    console.log(lessonsAllAux)
+    // console.log(lessonw1)
+}
+
+
+lessonsAll([lesson1, lesson2, lesson3])
 
