9dc6022a6b5f07348c095a86d83f9e3dbaef3226
Jadson Cerqueira <jadsoncerqueira97@gmail.com>
2022-05-29 17:30:39 -0300

    retorna um array com todos os objetos que possuem gênero ficção científica ou fantasia.

--- 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercicio2.js
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercicio2.js
@@ -1,4 +1,4 @@
-//  Construa um array de objetos a partir do array de livros. Cada objeto deve conter 
+//  retorna um array de objetos a partir do array de livros. Cada objeto contem
 // uma propriedade author, com o nome da pessoa autora do livro, e uma propriedade age 
 // com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, 
 //ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
