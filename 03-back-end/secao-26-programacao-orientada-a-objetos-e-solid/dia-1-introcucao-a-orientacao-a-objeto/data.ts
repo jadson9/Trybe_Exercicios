05788bc4cad00b4a1989392fcd99a485562d1e2c
Jadson Cerqueira <jadsoncerqueira97@gmail.com>
2022-11-10 22:24:59 -0300

    exercicio 5 - Criando classe Data

---
+++ 03-back-end/secao-26-programacao-orientada-a-objetos-e-solid/dia-1-introcucao-a-orientacao-a-objeto/data.ts
@@ -0,0 +1,10 @@
+class Data {
+  private _dia: string;
+  private _mes: string;
+  private _ano: string;
+  constructor(dia: string, mes: string, ano: string) {
+    this._dia = dia;
+    this._mes = mes;
+    this._ano = ano;
+  }
+}
