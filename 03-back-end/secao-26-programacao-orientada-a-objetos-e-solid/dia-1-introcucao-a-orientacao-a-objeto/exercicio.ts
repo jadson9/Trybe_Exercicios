3795e224bec5e5571bd66934059df50d30acb09a
Jadson Cerqueira <jadsoncerqueira97@gmail.com>
2022-11-10 21:27:23 -0300

    exercicio 2 - criando metodo de soma das notas e media

--- 03-back-end/secao-26-programacao-orientada-a-objetos-e-solid/dia-1-introcucao-a-orientacao-a-objeto/exercicio.ts
+++ 03-back-end/secao-26-programacao-orientada-a-objetos-e-solid/dia-1-introcucao-a-orientacao-a-objeto/exercicio.ts
@@ -1,19 +1,52 @@
-interface notasProvas {
-    nota1: number;
-    nota2: number;
-    nota3: number;
-    nota4: number;
+interface NotasProvas {
+  nota1: number;
+  nota2: number;
+  nota3: number;
+  nota4: number;
 }
 
-interface notasTrabalho {
-    nota1: number;
-    nota2: number;
+interface NotasTrabalho {
+  nota1: number;
+  nota2: number;
+}
+
+
+interface Params {
+  matricula: number;
+  nome: string;
+  notasDeProvas: NotasProvas;
+  notasDeTrabalho: NotasTrabalho;
 }
 
 
 class estudantes {
-  matricula: number;
-  nome: string;
-  notasDeProvas: notasProvas;
-  notasDeTrabalhos: notasTrabalho;
+  private matricula: number;
+  private nome: string;
+  private notasDeProvas: NotasProvas;
+  private notasDeTrabalhos: NotasTrabalho;
+
+  constructor(params: Params) {
+    this.matricula = params.matricula;
+    this.nome = params.nome;
+    this.notasDeProvas = params.notasDeProvas;
+    this.notasDeTrabalhos = params.notasDeTrabalho;
+  }
+
+  get soma(): number {
+    const notasMescladas = [
+      ...Object.values(this.notasDeProvas),
+      ...Object.values(this.notasDeTrabalhos),
+    ];
+    const soma = notasMescladas.reduce((acc, number) => acc + number);
+    return soma;
+  }
+
+  get media(): number {
+    const total = this.soma;
+    const provas = Object.values(this.notasDeProvas).length;
+    const trabalhos = Object.values(this.notasDeTrabalhos).length;;
+    const quantiNotas = provas + trabalhos;
+    return total / quantiNotas;
+
+  }
 }
