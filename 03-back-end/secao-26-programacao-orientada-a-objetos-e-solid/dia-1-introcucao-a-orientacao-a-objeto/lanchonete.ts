b1a6098517b0ce07692845c260f4395bcac6c8de
Jadson Cerqueira <jadsoncerqueira97@gmail.com>
2022-11-10 22:16:05 -0300

    exercicio 4 - criando 2 metodos para a classe de pedido, total de pedidos e total com desconto

--- 03-back-end/secao-26-programacao-orientada-a-objetos-e-solid/dia-1-introcucao-a-orientacao-a-objeto/lanchonete.ts
+++ 03-back-end/secao-26-programacao-orientada-a-objetos-e-solid/dia-1-introcucao-a-orientacao-a-objeto/lanchonete.ts
@@ -14,17 +14,32 @@ class Produto {
     this._nome = nome;
     this._preco = preco;
   }
+
+  get preco(): number {
+    return this._preco;
+  }
 };
 
-class Pedidos {
+class Pedido {
   private _cliente: string;
-  private _itensConsumidos: string[];
+  private _itensConsumidos: Produto[];
   private _formaPagamento: number
   private _desconto?: number;
-  constructor (cliente: string, itensConsumidos: string[], pagamento: number, desc?: number) {
+  constructor (cliente: string, itensConsumidos: Produto[], pagamento: number, desc?: number) {
     this._cliente = cliente;
     this._itensConsumidos = itensConsumidos;
     this._formaPagamento = pagamento;
     this._desconto = desc;
+  }
+
+  get total(): number {
+    const valores = this._itensConsumidos.map((el) => el.preco);
+    const total = valores.reduce((acc, num) => acc + num);
+    return total;
+  }
+
+  get totalComDesconto(): number {
+    const desco = this._desconto || 1
+    return this.total * desco;
   }
 };
